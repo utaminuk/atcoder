23781f1e6c84cd7b4e16d29d4c9be1b265dfa1ca
ku2ma2 <motorohi.tsumaniku@gmail.com>
2019-12-12 09:27:32 +0900

    [feat] コメントを追加

--- golang/abc/abc147_c/main.go
+++ golang/abc/abc147_c/main.go
@@ -27,12 +27,19 @@ func main() {
 		fmt.Printf("%d: %d\n", i, comments[i])
 	}
 
-	fmt.Println(members)
-	fmt.Println(types)
+	// bit全探索
+	// 2進数を左にSビット論理シフトすると、2^S倍することに相当
+	// 2 ^ ユーザー数 を計算して2bit的に全パターンを取得している
+	max_loop := 1 << count
+	for i := 0; i < max_loop; i++ {
+		// ok := true
 
-	// 全探索
-	iMax := 1 << count
-	fmt.Println(iMax)
+		// ユーザー数分ループする
+		for j := uint(0); j < count; j++ {
+			fmt.Printf("i: %d | j: %d\n", i, j)
+
+		}
+	}
 
 	// fmt.Printf("%d", result)
 
