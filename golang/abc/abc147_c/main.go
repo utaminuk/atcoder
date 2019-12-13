8c17b19dc1305a1a9b3957f8185755b01a6ef4a8
h.kunimatsu <h.kunimatsu@i-tap.co.jp>
2019-12-13 17:58:43 +0900

    [feat] bit全探索の練習追加
    bit全探索が分かり辛いので練習のためのメモ問題のようなものを追加している

--- golang/abc/abc147_c/main.go
+++ golang/abc/abc147_c/main.go
@@ -4,8 +4,22 @@ import (
 	"fmt"
 )
 
+// 正直者チェック
 func honest(number uint, digit uint) uint {
 	return (number >> digit) & 1
+}
+
+// 正直者カウント
+func honetsNum(n uint) uint {
+	result := uint(0)
+	for i := 0; n > 0; i++ {
+		// 末尾が1 だったらresultに1追加する
+		if n&1 == 1 {
+			result++
+		}
+		n >>= 1 // 一桁ずらす
+	}
+	return result
 }
 
 func main() {
@@ -28,20 +42,34 @@ func main() {
 			fmt.Scan(&members[i][j], &types[i][j])
 			members[i][j]-- // メンバーIDは-1してラベルと同じにする
 		}
-		fmt.Printf("%d: %d\n", i, comments[i])
 	}
 
 	// bit全探索
 	// 2進数を左にSビット論理シフトすると、2^S倍することに相当
 	// 2 ^ ユーザー数 を計算して2bit的に全パターンを取得している
-	max_loop := 1 << count
-	for i := 0; i < max_loop; i++ {
-		// ok := true
+	max_loop := uint(1) << count
+	// answer := uint(0)
+
+	for i := uint(0); i < max_loop; i++ {
+
+		ok := true
 
 		// ユーザー数分ループする
 		for j := uint(0); j < count; j++ {
+			if honest(i, j) == 0 {
+				continue
+			}
 			fmt.Printf("i: %d | j: %d\n", i, j)
 
+			for k := uint(0); k < comments[j]; k++ {
+				if honest(i, uint(members[j][k])) != types[j][k] {
+					ok = false
+					break
+				}
+			}
+			if !ok {
+				break
+			}
 		}
 	}
 
