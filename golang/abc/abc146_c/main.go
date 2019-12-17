a1c87ccbc5bc76f92368dcb72df2f751e27b349c
ku2ma2 <motorohi.tsumaniku@gmail.com>
2019-12-17 23:39:06 +0900

    [feat] abc146cを追加

--- golang/abc/abc146_c/main.go
+++ golang/abc/abc146_c/main.go
@@ -2,40 +2,32 @@ package main
 
 import (
 	"fmt"
-	"math"
 	"strconv"
 )
 
-func price(a, b, num uint) uint {
-	d := uint(len(strconv.Itoa(int(num))))
-	return a*num + b*d
+func price(a, b, num int) int {
+	return (a * num) + (b * len(strconv.Itoa(num)))
 }
 
 func main() {
 
-	var a uint
-	var b uint
-	var x float64
+	var a, b, x int
 	fmt.Scan(&a, &b, &x)
 
-	start := uint(0)
-	current := uint(math.Pow(10.0, x))
-	for {
-		current = (current - start) / 2
+	start := 0
+	end := int(1e9) + 1
+	center := 0
 
-		if uint(x) > price(a, b, current) {
-			// 買えた場合
-			start = current
+	for (end - start) > 1 {
+		center = (start + end) / 2
+
+		if price(a, b, center) <= x {
+			start = center
 		} else {
-			// 買えなかった場合
-			current /= 2
+			end = center
 		}
 
-		fmt.Printf("start: %d, current: %d\n", start, current)
-		if start == current {
-			break
-		}
 	}
-	fmt.Printf("%d %d %d\n", a, b, current)
+	fmt.Printf("%d", start) // startが買える値
 
 }
