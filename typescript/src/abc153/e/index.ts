98ca30c6e8be6111b3c7b1f6c522606eec8134e1
ku2ma2 <motorohi.tsumaniku@gmail.com>
2020-01-29 07:57:10 +0900

    [feat] abc153 eの回答を追加

--- typescript/src/abc153/e/index.ts
+++ typescript/src/abc153/e/index.ts
@@ -1,17 +1,44 @@
 import * as fs from 'fs';
 
+// 個数制限なし ナップザック問題
+// dp[i+1][j+KAi] = min( dp[i+1][j+KAi], dp[i][j] + KBi)
+type Magic = {
+  a: number;
+  b: number;
+};
+
 function main(input: string) {
 
-
   const lines = input.split("\n");
-  const [n, k] = lines[0].split(' ').map((v: string) => +v);
+  let [h, n] = lines[0].split(' ').map((v: string) => +v);
+  let magic: Magic[] = [];
 
-  const h = lines[1].split(' ')
-    .map((v: string) => +v)
-    .sort((a: number, b: number) => b - a)
-    .slice(k);
+  for (let i = 0; i < n; i++) {
+    const [a, b] = lines[i + 1].split(' ').map((v: string) => +v);
+    magic[i] = { a, b }
+  }
 
-  console.log(h.reduce((prev: number, current: number) => prev + current, 0));
+
+  const sorter = (magic: Magic[], h: number): Magic[] => {
+    return magic.sort((a, b) => {
+      const aH = (h > a.a) ? a.a : h;
+      const bH = (h > b.a) ? b.a : h;
+      return ((aH / a.b) > (bH / b.b)) ? -1 : 1;
+    });
+  };
+
+  let result = 0;
+  while (h > 0) {
+    magic = sorter(magic, h);
+    console.log(magic[0]);
+    console.log(h);
+    h -= magic[0].a;
+    result += magic[0].b;
+  }
+
+  console.log(result);
+
+
 }
 
 main(fs.readFileSync('/dev/stdin', 'utf8'));
