7daada43fad9e106ff447e5935ad910bc6bb64c4
utaminuk <motorohi.tsumaniku@gmail.com>
2020-02-03 13:59:08 +0900

    [feat] dp aの回答を追加

--- typescript/src/dp/a/index.ts
+++ typescript/src/dp/a/index.ts
@@ -11,12 +11,21 @@ function main(input: string) {
   const [n] = split_number(lines[0]);
 
   const h: number[] = split_number(lines[1]);
+  let dp: number[] = new Array(n).fill(Infinity);
+  dp[0] = 0;
 
-  const result = h.reduce((dp: number, bar: number, i: number) => {
-    return dp + bar;
-  }, 0);
+  let result: number = 0;
 
-  console.log(result);
+  for (let i = 0; i < n; i++) {
+    if (i >= 2) {
+      dp[i] = Math.min(dp[i], dp[i - 2] + Math.abs(h[i] - h[i - 2]));
+    }
+    if (i >= 1) {
+      dp[i] = Math.min(dp[i], dp[i - 1] + Math.abs(h[i] - h[i - 1]));
+    }
+  }
+
+  console.log(dp[n - 1]);
 }
 
 main(fs.readFileSync("/dev/stdin", "utf8"));
