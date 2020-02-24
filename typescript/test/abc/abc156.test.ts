589aff97456917e3b97ab3946c0f790d1fdb4f62
ku2ma2 <motorohi.tsumaniku@gmail.com>
2020-02-25 08:00:16 +0900

    [feat] abc156用のテンプレートを追加

---
+++ typescript/test/abc/abc156.test.ts
@@ -0,0 +1,62 @@
+import { runTest, AtcoderAnswers } from "../common";
+import { main as mainA } from "../../src/abc156/a";
+import { main as mainB } from "../../src/abc156/b";
+import { main as mainC } from "../../src/abc156/c";
+import { main as mainD } from "../../src/abc156/d";
+import { main as mainE } from "../../src/abc156/e";
+import { main as mainF } from "../../src/abc156/f";
+const CONTEST = `abc156`;
+
+describe(`${CONTEST} - A`, () => {
+  const json = `
+    [["0","0"],["0","0"]]
+    `;
+  const params: AtcoderAnswers = JSON.parse(json);
+  const customParams: AtcoderAnswers = [];
+  runTest(mainA, params, customParams);
+});
+
+describe(`${CONTEST} - B`, () => {
+  const json = `
+    [["0","0"],["0","0"]]
+    `;
+  const params: AtcoderAnswers = JSON.parse(json);
+  const customParams: AtcoderAnswers = [];
+  runTest(mainB, params, customParams);
+});
+
+describe(`${CONTEST} - C`, () => {
+  const json = `
+    [["0","0"],["0","0"]]
+    `;
+  const params: AtcoderAnswers = JSON.parse(json);
+  const customParams: AtcoderAnswers = [];
+  runTest(mainC, params, customParams);
+});
+
+describe(`${CONTEST} - D`, () => {
+  const json = `
+    [["0","0"],["0","0"]]
+    `;
+  const params: AtcoderAnswers = JSON.parse(json);
+  const customParams: AtcoderAnswers = [];
+  runTest(mainD, params, customParams);
+});
+
+describe(`${CONTEST} - E`, () => {
+  const json = `
+    [["0","0"],["0","0"]]
+    `;
+  const params: AtcoderAnswers = JSON.parse(json);
+  const customParams: AtcoderAnswers = [];
+  runTest(mainE, params, customParams);
+});
+
+describe(`${CONTEST} - F`, () => {
+  const json = `
+    [["0","0"],["0","0"]]
+    `;
+  const params: AtcoderAnswers = JSON.parse(json);
+  const customParams: AtcoderAnswers = [];
+  runTest(mainF, params, customParams);
+});
